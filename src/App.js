5974820245c61da5b2120c93e55471d0c887f695
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-08 08:58:55 -0500

    added props to sub-components, passed to main component App

--- src/App.js
+++ src/App.js
@@ -2,37 +2,44 @@ import React from 'react'
 import './App.css';
 
 // This is a sub component of the App component
-function Header() {
+function Header(props) { // the props object holds all the properties for the component
+  console.log(props)
   return (
     <header>
-      <h1>Wayne's Code Kitchen</h1>
+      <h1>{ props.name }'s Code Kitchen</h1>
     </header>
   )
 }
 // This is a sub component of the App component
-function Main() {
+function Main(props) {
+  console.log(props)
   return (
     <section>
-      <p>We serve a lot of delicious code recipes</p>
+      <p>We serve a lot of { props.adjective } code recipes</p>
     </section>
   )
 }
 // This is a sub component of the App component
-function Footer() {
+function Footer(props) {
+  console.log(props)
   return (
     <footer>
-      <p>True story</p>
+      <p>Copyright - { props.year } </p>
     </footer>
   )
 }
 
 // This is the main component of App.js
 const App = () => {
+  let options = { month: 'long' };
+  let month = new Date().toLocaleString('default', { month: 'long' })
+  let day = new Date().getDate()
+  let year = new Date().getFullYear()
   return (
     <div className="App">
-      <Header />
-      <Main />
-      <Footer />
+      <Header name="Ariana" />
+      <Main adjective="Awesome" />
+      <Footer year={ `${month} ${day}, ${year}` } />
     </div>
   );
 }
