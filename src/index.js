5aeddcd4c2fbff498397191c5657c41d31116385
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-08 13:59:07 -0500

    returned index back to normal state

--- src/index.js
+++ src/index.js
@@ -3,17 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-function AppTwo() {
-  return (
-    <h1>This is the second App</h1>
-  )
-}
-
 ReactDOM.render(
-  // Can use <></> tags in place of div tags
-  <>
-    <App />
-    <AppTwo />
-  </>,
+  <App />,
   document.getElementById('root')
 );
