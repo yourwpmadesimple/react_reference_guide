9992468c13804de516ceced8a708970e37d53ee7
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-08 06:55:13 -0500

    render elements using JSX

--- src/index.js
+++ src/index.js
@@ -4,14 +4,12 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  React.createElement(
-    "ul",
-    null,
-    React.createElement("li", null, "Monday"),
-    React.createElement("li", null, "Tuesday"),
-    React.createElement("li", null, "Wednesday"),
-    React.createElement("li", null, "Thursday"),
-    React.createElement("li", null, "Friday")
-  ),
+  <ul>
+    <li>Monday</li>
+    <li>Tuesday</li>
+    <li>Wednesday</li>
+    <li>Thursday</li>
+    <li>Friday</li>
+  </ul>,
   document.getElementById('root')
 );
