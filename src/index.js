96ee5c81efe7ded8f4e47a836ee6d59a87493702
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-08 05:52:57 -0500

    initial commit for 01_creatingReactElemets branch, switched out favicon

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  React.createElement('h1', null, "Heyyyy Wayne"),
   document.getElementById('root')
 );
