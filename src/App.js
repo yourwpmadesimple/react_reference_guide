1098444e35a4328b7edb753c318a088137928814
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-09 12:01:57 -0500

    initial commit, 098_configuringReactRouter branch

--- src/App.js
+++ src/App.js
@@ -1,33 +1,10 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import './index.css';
 import './App.css';
 
 
-const App = ({ login }) => {
-  const [ data, setData ] = useState(null)
+const App = () => {
 
-  useEffect(() => {
-    const url = `https://api.github.com/users/${login}`
-    fetch(`${url}`, {
-      method: "GET",
-      headers: {
-        Authorization: "token: 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"
-      }
-    })
-      .then(res => res.json())
-      .then(setData)
-  }, [])
-
-  if (data) {
-    console.log(data)
-    return <div>{ JSON.stringify(data) }</div>
-  }
-
-  return (
-    <div className="App">
-      <h1>Fetching Data with Hooks</h1>
-    </div>
-  )
 }
 
 export default App;
