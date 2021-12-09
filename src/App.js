29f93e96c83e8a08e90b2ca51fdb4a07c351739d
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-09 04:18:36 -0500

    initial commit, 093_useEffectHook branch, added first useEffect hook in App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import { React, useState } from 'react'
+import { React, useState, useEffect } from 'react'
 import './index.css';
 import './App.css';
 
 
 const App = () => {
   const [ emotion, setEmotion ] = useState('Happy')
+
+  useEffect(() => {
+    console.log(`It's ${emotional} around here`)
+  })
+
   return (
     <>
-      <h1>The current emotion is { emotion } </h1>
+      <h1>The current emotion is { emotion } ? </h1>
+      <button
+        className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+        onClick={ () => setEmotion('Happy') }>
+        Happy
+      </button>
       <button
         className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
         onClick={ () => setEmotion('Frustration') }>
