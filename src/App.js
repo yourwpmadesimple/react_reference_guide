145205b69dc9e547f5e3c445768a36d7b5b16ad9
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-09 08:08:42 -0500

    initial commit, 095_fetchDataWithHooks branch, setup fetch metod

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import React, { useReducer } from 'react'
+import React, { useState, useEffect } from 'react'
 import './index.css';
 import './App.css';
 
 
-const App = () => {
-  const [ checked, toggle ] = useReducer(
-    checked => !checked,
-    false
-  )
+const App = ({ login }) => {
+  const [ data, setData ] = useState(null)
+
+  useEffect(() => {
+    const url = `https://api.github.com/users/${login}`
+    fetch(`${url}`, {
+      method: "GET",
+      headers: {
+        Authorization: "token: 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"
+      }
+    })
+      .then(res => res.json())
+      .then(setData)
+  }, [])
+
+  if (data) {
+    console.log(data)
+    return <div>{ JSON.stringify(data) }</div>
+  }
 
   return (
-    <>
-      <input
-        type="checkbox"
-        value={ checked }
-        onChange={ toggle } />
-      <p>{ checked ? "Checked" : "Not Checked" }</p>
-    </>
+    <div className="App">
+      <h1>Fetching Data with Hooks</h1>
+    </div>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <App />,
+  <App login="yourwpmadesimple" />,
   document.getElementById('root')
 );
