849171c1f9518a3b56aa5f1ab7b6e8598fd2b06d
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-08 20:01:14 -0500

    created checkList array variable

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+const checkList = [ 'boots', 'tent', 'headlamp' ]
+console.log(checkList[ 1 ])
+
 ReactDOM.render(
   <App authorized={ true } />,
   document.getElementById('root')
