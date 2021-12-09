a9a6ab300ec3666cf0a51b54099ec76c06cd52c8
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-08 19:40:16 -0500

    using if statement for conditional logic

--- src/App.js
+++ src/App.js
@@ -1,65 +1,22 @@
 import React from 'react'
 import './index.css';
 import './App.css';
-import reacBanner from './react.jpg'
 
-// This is a sub component of the App component
-function Header(props) { // the props object holds all the properties for the component
-  // console.log(props)
-  return (
-    <header className="m-10">
-
-      <h1>{ props.name }'s Code Kitchen</h1>
-    </header>
-  )
-}
-// This is a sub component of the App component
-function Main(props) {
-  //console.log(props)
-  return (
-    <section>
-      <img className="object-none h-96 w-1/3 inline-block text-center" src={ reacBanner } alt="React JS Banner" />
-      <p className="text-3xl pt-10">We serve a lot of { props.adjective } code recipes</p>
-      <ul className="text-left">
-        { props.languages.map(language => (
-          <li key={ language.id }>{ language.language }</li>)) }
-      </ul>
-    </section>
-  )
-}
-// This is a sub component of the App component
-function Footer(props) {
-  // console.log(props)
-  return (
-    <footer>
-      <p>Copyright - { props.year } </p>
-    </footer>
-  )
+function SecretComponent() {
+  return (<h1>Secret informationn for authorized users only</h1>)
 }
 
-const languages = [
-  "React JS",
-  "JavaScript",
-  "Node",
-  "jQery"
-]
-//languages.map(language => console.log(language))
+function RegularComponent() {
+  return (<h1>Everyone can see this component</h1>)
+}
 
-const languageObjects = languages.map((language, i) => ({ id: i, language: language }))
-//console.log(languageObjects)
 
-// This is the main component of App.js
-const App = () => {
-  let month = new Date().toLocaleString('default', { month: 'long' })
-  let day = new Date().getDate()
-  let year = new Date().getFullYear()
-  return (
-    <div className="App">
-      <Header name="Ariana" />
-      <Main adjective="Awesome" languages={ languageObjects } />
-      <Footer year={ `${month} ${day}, ${year}` } />
-    </div>
-  );
+const App = (props) => {
+  if (props.authorized) {
+    return <SecretComponent />
+  } else {
+    return <RegularComponent />
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <App />,
+  <App authorized={ true } />,
   document.getElementById('root')
 );
