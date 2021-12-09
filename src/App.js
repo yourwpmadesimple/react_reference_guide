1157b44b253357e3a809527daf07bed02087c0e9
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-09 05:33:26 -0500

    added state, checkbox, and checkbox logic

--- src/App.js
+++ src/App.js
@@ -4,32 +4,15 @@ import './App.css';
 
 
 const App = () => {
-
+  const [ checked, setChecked ] = useState(false)
 
   return (
     <>
-      <h1>The current emotion is { emotion } and { secondary }  </h1>
-      <button
-        className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
-        onClick={ () => setSecondary('Crabby') }>
-        Make Crabby
-      </button>
-
-      <button
-        className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
-        onClick={ () => setEmotion('Happy') }>
-        Make Happy
-      </button>
-      <button
-        className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
-        onClick={ () => setEmotion('Frustration') }>
-        Frustrated
-      </button>
-      <button
-        className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
-        onClick={ () => setEmotion('Enthusiastic') }>
-        Enthusiastic
-      </button>
+      <input
+        type="checkbox"
+        value={ checked }
+        onChange={ () => setChecked(checked => !checked) } />
+      <p>{ checked ? "Checked" : "Not Checked" }</p>
     </>
   )
 }
